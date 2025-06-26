f2b9e73a94cdf5329db0a0ff27e3a305d8f11905
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-26 12:34:47 -0400

    Refactor bubble-sort method for readability

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,29 +1,25 @@
 def bubble_sort(array)
   # Take in an unsorted array
   # Return a sorted version using bubble sort
-  sorted_array = array
-  is_sorted = false
+  sorted_array = array.dup
+  array_length = array.length
   check_count = 0
 
-  until is_sorted do
+  loop do
     has_swapped = false
+    check_count += 1
+    unsorted_until_index = array_length - 1 - check_count
 
-    sorted_array.each_index do |index|
-      break if index + 1 == sorted_array.length - check_count
-
+    0.upto(unsorted_until_index) do |index|
       current_num = sorted_array[index]
       next_num = sorted_array[index + 1]
 
       if current_num > next_num
-        sorted_array[index] = next_num
-        sorted_array[index + 1] = current_num
+        sorted_array[index], sorted_array[index + 1] = next_num, current_num
         has_swapped = true
       end
     end
 
-    check_count += 1
-    is_sorted = true if !has_swapped
+    return sorted_array if !has_swapped
   end
-
-  sorted_array
 end
